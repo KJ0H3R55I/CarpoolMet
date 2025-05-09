582898f73e410a6857304a989541f3a855b9cf69
luqmanmoe <163691583+luqmanmoe@users.noreply.github.com>
2025-05-09 11:55:51 +0100

    Add files via upload
    register page

--- register.java
+++ register.java
@@ -16,7 +16,7 @@ public class register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_page);
+        setContentView(R.layout.activity_main);
 
         ImageButton homeButton = findViewById(R.id.home);
         homeButton.setOnClickListener(new View.OnClickListener() {
